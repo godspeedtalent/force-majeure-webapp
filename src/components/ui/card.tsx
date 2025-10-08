1e3bc46b53dfc7e2080712b3058e80a0cd475950
godspeedtalent <management@godspeedtalent.com>
2025-10-08 13:16:34 -0500

    straggling changes

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -95,9 +95,9 @@ CardFooter.displayName = 'CardFooter';
 
 export {
   Card,
-  CardHeader,
-  CardFooter,
-  CardTitle,
-  CardDescription,
   CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 };
